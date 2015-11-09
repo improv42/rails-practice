66ca71d011feb7436feae3fc526094f66d2dd91e
Ryan Hageman <improv42@gmail.com>
2015-11-09 09:30:26 -0600

    Annotated models with schema

--- music_db/app/models/artist.rb
+++ music_db/app/models/artist.rb
@@ -3,3 +3,18 @@ class Artist < ActiveRecord::Base
   has_many :songs
   belongs_to :genre
 end
+
+# == Schema Information
+#
+# Table name: artists
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  genre_id   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_artists_on_genre_id  (genre_id)
+#
